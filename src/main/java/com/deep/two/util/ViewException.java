263bb5ddda466feac56847351f519b2553c10ca6
sunxijin <sunxijin@sunxijin-pc>
2017-04-06 00:13:51 +0800

    提交

---
+++ src/main/java/com/deep/two/util/ViewException.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2014,TravelSky. 
+ * All Rights Reserved.
+ * TravelSky CONFIDENTIAL
+ * 
+ * Project Name:fare-util
+ * Package Name:com.travelsky.fare.airtisfaretoolutils.exception.util
+ * File Name:FareUtilException.java
+ * Date:2014-7-9 下午1:35:01
+ * 
+ */
+package com.deep.two.util;
+
+public class ViewException extends Exception {
+
+    public ViewException() {
+        super();
+    }
+
+    public ViewException(String msg) {
+        super(msg);
+    }
+
+    public ViewException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public ViewException(Throwable cause) {
+        super(cause);
+    }
+}
